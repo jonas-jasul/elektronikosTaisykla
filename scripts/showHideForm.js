62877ef3d0a61a9b50ed11303b54ee031fdadcf7
jonas-jasul <jonas.jasul@gmail.com>
2022-09-16 18:02:10 +0300

    added js script to hide/show form

---
+++ scripts/showHideForm.js
@@ -0,0 +1,13 @@
+const btn= document.getElementById('btn-show');
+
+btn.addEventListener('click', () => {
+    const form = document.getElementById('form');
+
+    if(form.style.display==='none') {
+        form.style.display='block';
+    }
+
+    else {
+        form.style.display='none';
+    }
+});
